305e39513d4561c413d37b8c77cd412e4c8a9746
tvlad <tvl71@mail.ru>
2016-02-08 17:40:11 +0200

    added verification for case when there is not a product name in a prompt Folder 31

--- lib/31/add_to_cart.rb
+++ lib/31/add_to_cart.rb
@@ -2,8 +2,17 @@ require_relative "Init"
 
 cart = Cart.new(ARGV.delete_at(0))
 
+
+if ARGV.size == 0 
+#    @items.each { |i| cart.add_item(i) if i.name == "noname" }
+puts "You should input a product name"
+end
+
+
 ARGV.each do |a|
-  @items.each { |i| cart.add_item(i) if a == i.name}
+  
+  @items.each { |i| cart.add_item(i) if a == i.name }
+  
 end
 
 cart.read_from_file
