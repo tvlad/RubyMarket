810b94ea02085e00658176bff8438ac9c09a2026
tvlad <tvl71@mail.ru>
2016-02-08 17:40:36 +0200

    added verification for case when there is not a product name in a prompt Folder 31

--- lib/31/Init.rb
+++ lib/31/Init.rb
@@ -11,21 +11,21 @@ require_relative "Order"
 @items << RealItem.new({:name => "Spoon", :weight => 12, :price => 110})
 @items << RealItem.new({:name => "Dish", :weight => 14, :price => 250})
 @items << VirtualIem.new({:name => "Book", :price => 120})
-@items << RealItem.new({:price => 500})
+#@items << RealItem.new({:price => 500})
 #@items << VirtualIem.new(:name => "mp3", :price => 80)
 #@items << VirtualIem.new(:name => "avi", :price => 60)
 
 puts "-----------------------"
 
 @items.each { |i| p i,  i.class }
-
-puts "-----------------------"
-
-@items2 = []
-@items2 << Item.new({:name => "Folk", :weight => 10, :price => 100})
-@items2 << Item.new({:name => "Spoon", :weight => 12, :price => 110})
-@items2 << Item.new({:name => "Dish", :weight => 14, :price => 250})
-@items2 << Item.new(:name => "Book", :price => 120)
-@items2 << Item.new({:price => 500})
-
-@items2.each { |i| p i }
+#
+#puts "-----------------------"
+#
+#@items2 = []
+#@items2 << Item.new({:name => "Folk", :weight => 10, :price => 100})
+#@items2 << Item.new({:name => "Spoon", :weight => 12, :price => 110})
+#@items2 << Item.new({:name => "Dish", :weight => 14, :price => 250})
+#@items2 << Item.new(:name => "Book", :price => 120)
+#@items2 << Item.new({:price => 500})
+#
+#@items2.each { |i| p i }
